73d062e3ec9ad2f21e1c16ef15cfeefb05d368d5
Christian Stevanus <chris.stev30@gmail.com>
2019-02-15 16:45:16 +0700

    Initial Commit

---
+++ Ma'Flawa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ma'Flawa
+//
+//  Created by Christian Stevanus on 15/02/19.
+//  Copyright © 2019 Christian Stevanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
